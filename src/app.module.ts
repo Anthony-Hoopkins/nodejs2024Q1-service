5847b39dd6dce16e968626697052a1d8a92146a4
Anthony M <aantonij@gmail.com>
2024-03-09 14:51:21 +0300

    init: create structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserResolver } from './user/user.resolver';
+import { UserModule } from './user/user.module';
+import { ArtistModule } from './artist/artist.module';
+import { TrackModule } from './track/track.module';
+import { AlbumModule } from './album/album.module';
+import { FavoritesModule } from './favorites/favorites.module';
 
 @Module({
-  imports: [],
+  imports: [
+    UserModule,
+    ArtistModule,
+    TrackModule,
+    AlbumModule,
+    FavoritesModule,
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, UserResolver],
 })
 export class AppModule {}
